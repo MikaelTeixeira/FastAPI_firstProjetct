4963dbbef03c8a90a448cc0de8c8b067fa219d38
Mikael Teixeira <142433000+MikaelTeixeira@users.noreply.github.com>
2026-02-17 20:44:37 -0300

    structure Update

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from auth_routes import auth_router
+from order_routes import order_router
+
+
+app.include_router(auth_router)
+app.include_router(order_router)
+
+#para rodar o servidor digitar no terminal: uvicorn main:app --reload
+
+# Uvicorn é a biblioteca responsável por fazer o servidor ir ao ar
+
+#endpoint:  
+#/ordens
+#é o resto do caminho após o domínio do site.
+
+
+#REST API's
+# Get --> Leitura/Pegar informação
+# Post --> Enviar/Criar informação
+# put/patch --> Editar informação
+# Delete --> Deletar informação
